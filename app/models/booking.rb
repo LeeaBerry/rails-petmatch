c8b825561aa34b3c04632757b59b314cd21cd027
madhusudhan kandukuri <madhusudhan.kandukuri@gmail.com>
2025-01-21 16:00:53 +0000

    added db-seeds

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,5 @@ class Booking < ApplicationRecord
   belongs_to :pet
 
   validates :booking_startdate, :booking_enddate, presence: true
-  validates :booking_startdate, comparison: { less_than: :end_date, message: "must be before the end date" }
+  validates :booking_startdate, comparison: { less_than: :booking_enddate, message: "must be before the end date" }
 end
